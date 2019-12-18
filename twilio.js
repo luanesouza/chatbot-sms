48c368eab4bb6fa07e819df96f00a2feaf205259
luanesouza <luanesouza411@gmail.com>
2019-12-17 20:25:02 -0500

    initial commit

---
+++ twilio.js
@@ -0,0 +1,5 @@
+// You have been hired to build a small contact center that sends out SMS messages.
+// The receiver of the message then replies and the inbound message would have to go through a chatbot.
+var twiml = new Twilio.twiml.MessagingResponse()
+
+console.log(twiml);
